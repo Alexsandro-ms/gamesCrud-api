c117c2699184aa75ee5236ceab8023b713455a20
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-03 18:23:37 -0300

    feat(routes): creating a route for viewing information about a game
    route for viewing information about a game by id

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,31 @@ router.get("/games", (req, res) => {
     .then((games) => {
       res.send(games).statusCode(200);
     })
-    .catch((error) => console.log(error));
+    .catch((err) =>
+      res.send({
+        error: true,
+        err
+      })
+    );
+});
+
+router.get("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    const id = parseInt(req.params.id);
+    GamesModel.findOne({
+      where: {
+        id
+      }
+    })
+      .then((game) => {
+        if (game.id != undefined) res.sendStatus(200);
+      })
+      .catch((error) => {
+        res.sendStatus(404);
+      });
+  }
 });
 
 module.exports = router;
