e85a8a33f209db8a3be6af4a470f6d5d95edeba0
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-03 21:37:51 -0300

    feat(routes): route for editing information already registered in games
    route for editing information already registered for games via id, information changed from new
    information coming from the client

--- src/routes.js
+++ src/routes.js
@@ -70,4 +70,40 @@ router.delete("/game/:id", (req, res) => {
     });
 });
 
+router.put("/game/:id", (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    const id = parseInt(req.params.id);
+    const { title, year, price } = req.body;
+
+    GamesModel.findByPk(id)
+      .then((game) => {
+        if (title != undefined) {
+          game.title = title;
+        }
+        if (year != undefined) {
+          game.year = year;
+        }
+        if (price != undefined) {
+          game.price = price;
+        }
+
+        GamesModel.update(
+          {
+            title,
+            year,
+            price
+          },
+          {
+            where: { id }
+          }
+        ).then(res.sendStatus(200));
+      })
+      .catch((err) => {
+        res.sendStatus(400);
+      });
+  }
+});
+
 module.exports = router;
