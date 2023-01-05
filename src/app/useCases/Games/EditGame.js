aa2e40ce6201f978cf40f49016ccc710db028a1b
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-01-04 22:03:16 -0300

    refactor(usecases/games/editgame): refactored field validation

--- src/app/useCases/Games/EditGame.js
+++ src/app/useCases/Games/EditGame.js
@@ -2,37 +2,39 @@ const GamesModel = require("../../models/GamesModel");
 
 const EditGame = (req, res) => {
   if (isNaN(req.params.id)) {
-    res.sendStatus(400);
+    return res.sendStatus(404);
   } else {
     const id = parseInt(req.params.id);
     const { title, year, price } = req.body;
 
-    GamesModel.findByPk(id)
-      .then((game) => {
-        if (title != undefined) {
+    try {
+      GamesModel.findByPk(id).then((game) => {
+        if (title != undefined || year != undefined || price != undefined) {
           game.title = title;
-        }
-        if (year != undefined) {
           game.year = year;
-        }
-        if (price != undefined) {
           game.price = price;
-        }
 
-        GamesModel.update(
-          {
-            title,
-            year,
-            price
-          },
-          {
-            where: { id }
-          }
-        ).then(res.sendStatus(200));
-      })
-      .catch((err) => {
-        res.sendStatus(400);
+          GamesModel.update(
+            {
+              title,
+              year,
+              price
+            },
+            {
+              where: { id }
+            }
+          )
+            .then(() => {
+              return res.sendStatus(200);
+            })
+            .catch((err) => {
+              return res.sendStatus(400);
+            });
+        }
       });
+    } catch (err) {
+      return res.sendStatus(404);
+    }
   }
 };
 
